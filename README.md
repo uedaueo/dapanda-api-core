1ba7f1633649933583f849c9c3877857c9e4c284
tueda <ueo@ueo.co.jp>
2022-08-30 19:07:18 +0900

    DB access issues: Get information from env or command line arguments.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 * Java11, mysqlが必要なのでインストール。
 * dapanda-api-coreには現状トークン認証のサンプルがあるのでDBの設定が必要。
 mysqlを使えるようにしdapanda-api-coreにあるsampleTable.sqlを使ってデータベースの作成を行う。
-* application.ymlのデータベースの設定をしている箇所(datasources.default)を環境に合わせて修正する。
+* ~~application.ymlのデータベースの設定をしている箇所(datasources.default)を環境に合わせて修正する。~~
+* DB 接続情報については、環境変数またはコマンドライン引数で指定する。
 * gradle.propertiesのデータベースの設定をしている箇所を環境に合わせて修正する。~~(blancoDbJdbcUrl, blancoDbJdbcUser, blancoDbJdbcPasswordを修正する)~~
   * gradle.properties を直接修正せずに、local.gradle に設定する
   * local.gradle は .gitignore に入れているので、設定変更をしない場合でも必ず空ファイルを作成すること
@@ -17,3 +18,60 @@ mysqlを使えるようにしdapanda-api-coreにあるsampleTable.sqlを使っ�
 ```
 ./gradlew publishToMavenLocal
 ```
+
+## DB 接続情報の設定方法
+DB 接続情報の設定は、環境変数または、コマンドラインで行える。
+AWS 環境上では、環境変数を利用する。
+ローカル環境では、コマンドラインを利用する。
+
+### 環境変数で設定する場合
+環境変数名は、`DB_SECRETS`となる。
+設定する値は、AWS Secrets Manager 形式で以下のように設定する。
+
+```json5
+{
+  "host": "192.168.1.1",
+  "port": "3306",
+  "username": "root",
+  "password": "password"
+}
+```
+
+環境変数を受け取る処理は、
+`dapanda.api.common.application.Application`
+に実装している。
+
+### コマンドラインで設定する場合
+以下のように実行する。
+```
+gradlew run -PjvmArgs="-Ddb.host=192.168.1.1 -Ddb.port=33360"
+```
+
+### 複数DBを使いたい場合
+
+Replication で検索専用 DB にアクセスする場合など。
+
+* application.yml の datasource: に、default: 以外の名前（たとえば readonly）で設定を追記する
+* dataSource を DI する際に、@Named("readonly") と指定する
+
+```kotlin
+@Prototype
+class UserRepository(
+    private val dataSource: DataSource,
+    @Named("readonly")
+    private val dataSourceReadOnly: DataSource
+) {
+    // .....
+}
+```
+
+## ビルドについて
+gradle でのクリーンビルドは、以下のように行う。
+この方法は、キャッシュやバックグランド実行を行わない。
+Micronaut の Bean 定義が、正常に生成されない場合などで、このコマンドを実行する。
+```
+gradlew --no-build-cache --no-daemon clean build
+```
+
+IntelliJ の場合は、`Run/Debug Configurations` の `VM options` に
+`-Dorg.gradle.caching=false -Dorg.gradle.daemon=false` を設定する。
--- src/main/kotlin/dapanda/api/common/application/Application.kt
+++ src/main/kotlin/dapanda/api/common/application/Application.kt
@@ -1,16 +1,107 @@
 package dapanda.api.common.application
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.context.env.PropertySource
 import io.micronaut.runtime.Micronaut.build
+
+/** DB 接続情報の環境変数 */
+private const val DB_SECRETS = "DB_SECRETS"
+
+/** Aurora エンドポイントの正規表現一致グループ数 */
+private const val AURORA_ENDPOINT_PATTERN_GROUP_COUNT = 3
 
 /**
  * アプリケーションの実行
  */
 object Application {
+
+	// DB 接続情報を取得
+	val additionalProperties =
+		getEscapedDbPasswordPropertySource()
+
 	@JvmStatic
 	fun main(args: Array<String>) {
 		build()
 			.args(*args)
 			.packages("dapanda.api")
+			.mainClass(Application.javaClass)
+			.propertySources(additionalProperties)
+			.environmentVariableExcludes(DB_SECRETS)
 			.start()
 	}
+}
+
+/**
+ * 環境変数に指定した Secrets Manager の DB 接続情報（JSON）を分解してプロパティソースに変換する。
+ *
+ * 現時点の ECS Fargate では、Secrets Manager の値（JSON）から個別の値（host など）を
+ * 取り出すことができない。そのため、ここで JSON 文字列を解析する処理を行う。
+ *
+ * @return エスケープ済みパスワードを設定するためのプロパティソースを返す。
+ */
+private fun getEscapedDbPasswordPropertySource(): PropertySource {
+	val map: MutableMap<String, Any> = mutableMapOf()
+	System.getenv(DB_SECRETS)?.also { dbSecretsEnv ->
+		val objectMapper = ObjectMapper()
+		objectMapper.readValue(dbSecretsEnv, DbSecrets::class.java).let { dbSecrets ->
+			dbSecrets.host?.also { host ->
+				map["db.host"] = host
+			}
+			dbSecrets.hostReadOnly?.also { hostReadOnly ->
+				map["db.hostRead"] = hostReadOnly
+			}
+			dbSecrets.port?.also { port ->
+				map["db.port"] = port
+			}
+			dbSecrets.username?.also { username ->
+				map["db.username"] = username
+			}
+			dbSecrets.password?.also { password ->
+				map["db.password"] = password
+			}
+		}
+	}
+	return PropertySource.of("escapedDbPassword", map)
+}
+
+/**
+ * DB 接続情報
+ *
+ * パスワードは、エスケープ処理をする
+ * パスワードに "${" を含む場合、Micronaut のプロパティホルダーと認識されエラーとなる。
+ * そのため、"${" を、プレースホルダ "${dummy:${}" に置換する。
+ * プレースホルダ "${dummy:${}" は、「dummy」がプロパティ名で、コロン以降（「${」）がデフォルト値となる。
+ * 「dummy」は、未設定とし、解決されない前提として、デフォルト値「${」に変換されるようにする。
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+class DbSecrets {
+	/** ホスト */
+	var host: String? = null
+
+	/** ポート */
+	var port: Int? = null
+
+	/** ユーザ名 */
+	var username: String? = null
+
+	/** パスワード */
+	var password: String? = null
+		get() = field?.replace("\${", "\${dummy:\${}")
+
+	/** 参照専用ホスト */
+	val hostReadOnly: String?
+		get() = host?.let { hostReadWrite ->
+			Regex("""([^.]+\.cluster)(-[^.]+.ap-northeast-1.rds.amazonaws.com)""")
+				.find(hostReadWrite)
+				?.groupValues
+				?.takeIf { it.size == AURORA_ENDPOINT_PATTERN_GROUP_COUNT }
+				?.let { groupValues ->
+					// Aurora のエンドポイント形式に一致してる場合、
+					// 参照専用エンドポイントの形式に置換する
+					// groupValues[0] には、全体の文字列が格納されている。
+					"${groupValues[1]}-ro${groupValues[2]}"
+				}
+				?: host
+		}
 }
